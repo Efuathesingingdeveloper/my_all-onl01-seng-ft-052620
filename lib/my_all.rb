de82c9f22676b4e93794e6ef87bbb7769b182602
Georgette Class-Peters <georgycp@udel.edu>
2020-06-03 19:10:36 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,9 +4,7 @@ def my_all?(collection)
   index= 0
   
   while index < collection.length
-   result = yield(collection[index])
+   if yield(collection[index])
    index += 1
   end
-  
-  return result
 end
